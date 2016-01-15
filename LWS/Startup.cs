d7feb6dac6092c6b3d0ba20f0b1ef9dff89f3f0f
tinreslutely <tinreslutely@hotmail.com>
2016-01-15 23:37:04 +0800

    initalize projects

---
+++ LWS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LWS.Startup))]
+namespace LWS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
